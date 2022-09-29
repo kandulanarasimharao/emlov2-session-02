8137b448547d28e679934db2d0d253e38b263a6d
Narasimha Rao Kandula CSE <narasimharao.k@vishnu.edu.in>
2022-09-29 16:52:42 +0000

    after changing all cpus to gpus

--- tests/test_train.py
+++ tests/test_train.py
@@ -13,7 +13,7 @@ def test_train_fast_dev_run(cfg_train):
     HydraConfig().set_config(cfg_train)
     with open_dict(cfg_train):
         cfg_train.trainer.fast_dev_run = True
-        cfg_train.trainer.accelerator = "cpu"
+        cfg_train.trainer.accelerator = "gpu"
     train(cfg_train)
 
 
@@ -34,7 +34,7 @@ def test_train_epoch_gpu_amp(cfg_train):
     HydraConfig().set_config(cfg_train)
     with open_dict(cfg_train):
         cfg_train.trainer.max_epochs = 1
-        cfg_train.trainer.accelerator = "cpu"
+        cfg_train.trainer.accelerator = "gpu"
         cfg_train.trainer.precision = 16
     train(cfg_train)
 
@@ -55,7 +55,7 @@ def test_train_ddp_sim(cfg_train):
     HydraConfig().set_config(cfg_train)
     with open_dict(cfg_train):
         cfg_train.trainer.max_epochs = 2
-        cfg_train.trainer.accelerator = "cpu"
+        cfg_train.trainer.accelerator = "gpu"
         cfg_train.trainer.devices = 2
         cfg_train.trainer.strategy = "ddp_spawn"
     train(cfg_train)
